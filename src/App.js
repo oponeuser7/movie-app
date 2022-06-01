c421dbfac6ec63d4c574f4f95569472763ba8550
lavgin7 <oponeuser7@gmail.com>
2022-06-01 18:04:10 +0900

    add search bar and table layout

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import './style.css';
 function App() {
   return (
     <div id="app">
-      <p>movie</p>
+      <input type="text" id="search-input" />
+      <button type="button" id="search-button">search!</button>
+      <div id="results-container">
+        <table id="results">
+          <tr>
+            <th>title</th>
+            <th>director</th>
+            <th>realse date</th>
+            <th>runtime</th>
+            <th>rating</th>
+            <th>genre</th>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
