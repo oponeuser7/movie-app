f8e5132879ff9a99685eba7502006a51258a1bce
lavgin7 <oponeuser7@gmail.com>
2022-06-16 14:26:52 +0900

    feat: book - 1

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,6 +1,7 @@
 import './style.css';
 import { useState, useEffect } from 'react';
-import { API, objToList } from './api';
+import { API, APIQS, objToList } from './api';
+import qs from 'qs';
 
 const Schedule = (props) => {
   const [list, setList] = useState(null);
@@ -24,6 +25,41 @@ const Schedule = (props) => {
     fetchSchedules();
   }, [props.currentTheater]);
 
+  const book = (e) => {
+    const post = async () => {
+      const sid = e.target.parentNode.nextSibling.innerHTML;
+      const uid = window.localStorage.getItem('uid');
+      if(!uid) {
+        alert('Sign in first!');
+        return;
+      }
+      const adult = parseInt(prompt('Adults', '0'));
+      const child = parseInt(prompt('Childs', '0'));
+      const seats = adult+child;
+      const fee = (10000*adult)+(7000*child);
+      const cash = parseInt(prompt('Cash', '0'));
+      const points = parseInt(prompt('Points', '0'));
+      if(fee>cash+points) {
+        alert('Not enough pay!');
+        return;
+      }
+      const bonus = parseInt(fee*0.05);
+      const body = {
+        sid: sid,
+        uid: uid,
+        seats: seats,
+        cash: cash,
+        points: points,
+        bonus: bonus
+      };
+      console.log(qs.stringify(body));
+      const response = await APIQS.post('book.jsp', qs.stringify(body));
+      console.log(response.data);
+      alert('Book Success!');
+    };
+    post();
+  };
+
   if(loading) return <div>Loading...</div>;
   if(!list) return null;
   if(list && list.length===0) return <div>No Schedule Found...</div>;
@@ -34,15 +70,17 @@ const Schedule = (props) => {
             <tr>
               <th>Auditorium</th>
               <th>Starting Time</th>
+              <th>Book</th>
             </tr>
           </thead>
           <tbody>
             {
               list.map((data, index) => (
               <tr key={index}>
-                <td style={{display:"none"}}>{data[0]}</td>
                 <td>{data[1]}</td>
                 <td className="button">{data[2]}</td>
+                <td><button type="button" onClick={book}>book</button></td>
+                <td style={{display:"none"}}>{data[0]}</td>
               </tr>
               ))
             }
