7b7ed2ffe021b516799efac3783c7bf5411ddc83
lavgin7 <oponeuser7@gmail.com>
2022-06-16 12:00:31 +0900

    feat: add API with query string

--- src/api.js
+++ src/api.js
@@ -7,10 +7,17 @@ const API = axios.create({
   }
 });
 
+const APIQS = axios.create({
+  baseURL: "//127.0.0.1:8080/movie/movie-server/",
+  headers: {
+    "Content-Type": "application/x-www-form-urlencoded",
+  }
+});
+
 const objToList = (obj) => {
   let temp = [];
   for(const key in obj) temp.push(Object.values(obj[key]));
   return temp;
 };
 
-export { API, objToList };
+export { API, APIQS, objToList };
