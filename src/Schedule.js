da6fe21e999975a9d9115f17b0168c1dbfe08f04
lavgin7 <oponeuser7@gmail.com>
2022-06-17 13:04:14 +0900

    feat: separate book logic

--- src/Schedule.js
+++ src/Schedule.js
@@ -1,7 +1,7 @@
 import './style.css';
 import { useState, useEffect } from 'react';
-import { API, APIQS, objToList } from './api';
-import qs from 'qs';
+import { API, objToList } from './api';
+import { book, updateSeats, updatePoints } from './bookLogic';
 
 const Schedule = (props) => {
   const [list, setList] = useState(null);
@@ -24,80 +24,6 @@ const Schedule = (props) => {
     }
     fetchSchedules();
   }, [props.currentTheater]);
-
-  const book = (e) => {
-    const post = async () => {
-      const sid = e.target.parentNode.nextSibling.innerHTML;
-      const uid = window.localStorage.getItem('uid');
-      const type = e.target.parentNode.previousSibling.previousSibling.innerHTML;
-      const seatLeft = e.target.parentNode.previousSibling.innerHTML;
-      if(!uid) {
-        alert('Sign in first!');
-        return;
-      }
-      const adult = parseInt(prompt('Adults', '0'));
-      const child = parseInt(prompt('Childs', '0'));
-      const seats = adult+child;
-      if(seats>seatLeft) {
-        alert('There are no seats more than '+seatLeft+'!');
-        return;
-      }
-      const adultFee = type==='premium' ? 15000 : 10000;
-      const childFee = type==='premium' ? 13000 : 8000;
-      const total = (adultFee*adult)+(childFee*child);
-      const cash = parseInt(prompt('Cash', '0'));
-      const points = parseInt(prompt('Points', '0'));
-      if(total>cash+points) {
-        alert('Not enough pay!');
-        return;
-      }
-      const bonus = parseInt(total*0.05);
-      const body = {
-        sid: sid,
-        uid: uid,
-        seats: seats,
-        cash: cash,
-        points: points,
-        bonus: bonus
-      };
-      await APIQS.post('book.jsp', qs.stringify(body));
-      updateSeats(sid, seats);
-      updatePoints(uid, bonus);
-      alert('Book Success!');
-      window.location.reload();
-    };
-    post();
-  };
-
-  const updateSeats = async (sid, seats) => {
-    const response = await API.get('getSeats.jsp', {
-      params: {
-        sid: sid
-      }
-    });
-    const currentSeats = parseInt(response.data[0]?.seats);
-    const newSeats = currentSeats-seats;
-    const body = {
-      sid: sid,
-      seats: newSeats
-    };
-    const response2 = await APIQS.post('postSeats.jsp', qs.stringify(body));
-  };
-
-  const updatePoints = async (uid, points) => {
-    const response = await API.get('getPoints.jsp', {
-      params: {
-        uid: uid
-      }
-    });
-    const currentPoints = parseInt(response.data[0]?.points);
-    const newPoints = currentPoints+points;
-    const body = {
-      uid: uid,
-      points: newPoints
-    };
-    const response2 = await APIQS.post('postPoints.jsp', qs.stringify(body));
-  };
 
   if(loading) return <div>Loading...</div>;
   if(!list) return null;
